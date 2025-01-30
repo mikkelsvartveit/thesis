c761064c7db9c92cf81f84ed4345de4ecc2c9ef2
Mikkel Svartveit <mikkel.svartveit@gmail.com>
2025-01-30 11:09:24 +0100

    Update Idun script

--- masterproject/code/start_idun_session.sh
+++ masterproject/code/start_idun_session.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# Define default values
-ACCOUNT="stianjsu"
+# Default configuration values
 NODES=1
-CPUS=4
+CPUS=8
 MEM="16G"
 TIME="01:00:00"
-PARTITION="GPUQ"  # Change this to your desired default partition
+PARTITION="GPUQ"
 
-# Script name for usage message
+# Get script name for usage message
 SCRIPT_NAME=$(basename "$0")
 
 # Help function
 usage() {
     echo "Usage: $SCRIPT_NAME [-t time] [-n nodes] [-c cpus] [-m memory] [-p partition]"
+    echo "Options:"
     echo "  -t: Wall time (default: $TIME)"
     echo "  -a: Account name (default: $ACCOUNT)"
     echo "  -n: Number of nodes (default: $NODES)"
@@ -27,19 +27,28 @@ usage() {
 # Parse command line options
 while getopts "t:a:n:c:m:p:h" opt; do
     case $opt in
-        t) TIME="$OPTARG" ;;
-        a) ACCOUNT="$OPTARG" ;;
-        n) NODES="$OPTARG" ;;
-        c) CPUS="$OPTARG" ;;
-        m) MEM="$OPTARG" ;;
-        p) PARTITION="$OPTARG" ;;
-        h) usage ;;
-        ?) usage ;;
+        t)  if [[ $OPTARG =~ ^[0-9]+$ ]]; then
+                # If just a number is provided, treat it as hours
+                TIME="$OPTARG:00:00"
+            else
+                # Otherwise use the provided time format
+                TIME="$OPTARG"
+            fi
+            ;;
+        n)  NODES="$OPTARG"
+            ;;
+        c)  CPUS="$OPTARG"
+            ;;
+        m)  MEM="$OPTARG"
+            ;;
+        p)  PARTITION="$OPTARG"
+            ;;
+        h)  usage
+            ;;
+        ?)  usage
+            ;;
     esac
 done
-
-
-
 
 # Launch salloc with specified parameters
 salloc \
@@ -48,4 +57,4 @@ salloc \
     --cpus-per-task="$CPUS" \
     --mem="$MEM" \
     --partition="$PARTITION" \
-    --gres="gpu:a100:1" 
+    --gres="gpu:a100:1"
